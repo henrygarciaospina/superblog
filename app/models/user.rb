0c0c33b40352fe64c03c823e9c2de6b171addd10
henrygarciaospina <henrygarcia0309@gmail.com>
2017-09-02 00:29:42 -0500

    Se agregó para users el atributo de confirmación de contraseña

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,18 @@
 #
 
 class User < ApplicationRecord
-  has_secure_password validations: false
+  has_secure_password validations: true
 
   has_many :posts
   has_many :comments
 
+  validates :email, presence: true, on: :create
   validates :email, uniqueness: true, format: /@/
+
   validates :password, presence: true, on: :create
   validates :password, length: { in: 6..20 }, allow_nil: true
+
+  validates :password_confirmation, presence: true, on: :create
+  validates :password_confirmation, length: { in: 6..20 }, allow_nil: true
+
 end
